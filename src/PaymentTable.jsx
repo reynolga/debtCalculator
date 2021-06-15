69e5eb76adf0a2124b1f705f4d92e7b0babfbc80
XanderReynolds <xane514@gmail.com>
2021-06-15 08:14:55 -0400

    show as a table instead

--- src/PaymentTable.jsx
+++ src/PaymentTable.jsx
@@ -2,22 +2,57 @@ import React from 'react';
 
 class PaymentTable extends React.Component {
 
+  calculateTotalInterest = (payments) => {
+    let result = payments.reduce((sum, {interest}) => sum + interest, 0);    
+    return result.toFixed(2);
+  }
+
+  estSeries = (payments) => {
+    let interest = payments.map(({interest}, i) => interest);
+    let result = [];
+    let sum = 0;
+    for (let i = 0; i < interest.length; i++){
+      sum += interest[i];
+      result = [...result, [i, sum]]
+    }
+    
+    return result;
+  };
+
+  calculateTotalPayment = (payments) => {
+    let result = payments.reduce((sum, {paymentAmount}) => sum + Number(paymentAmount), 0);    
+    return result.toFixed(2);
+  }
+
+  petPaymentsSeries = (payments) => {
+    let result = payments.map(({paymentAmount}, i) => [i, Number(paymentAmount)]);
+    return result;
+  }
+
 
   render() {
-    const {items} = this.props;
+    const {payments} = this.props;
+    let totalInterest = 0;
+    let totalPayments = 0;
+
+    console.log(payments);
+    if(payments){
+        totalInterest = this.calculateTotalInterest(payments);
+    }
+    if(payments){
+      totalPayments = this.calculateTotalPayment(payments);
+  }
 
      return (
-       <ul>
-         {items.map(item => (
-           <li key={item.id}>
-             <div>{item.remainingBalance}</div>  
-             <div>{item.paymentAmount}</div>  
-             <div>{item.interest}</div>  
-           </li>
-         ))}
-       </ul>
-     )
-  }
+       <div>
+        <div>Remaining Balance: {payments.remainingBalance}</div>  
+        <div>Total Payments: {totalPayments}</div>  
+        <div>Total Interest Paid: {totalInterest}</div>    
+      </div>         
+         )
+        }
+     
+  
   
 }
 
